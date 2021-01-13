332417b1353bc247052d656bf2da9640accd4496
Ivan Sanchez <ivan@gourame.com>
2021-01-13 15:59:26 +0000

    Display jobs for commit

--- src/main/js/CommitRuns.js
+++ src/main/js/CommitRuns.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {capitalize, replace, split, truncate} from "lodash/string";
 import {last} from "lodash/array";
+import moment from "moment";
 
 function prettifyName(runElement) {
     return capitalize(replace(replace(last(split(runElement, '/')), '.yaml', ''), '_', ' '))
@@ -10,11 +11,12 @@ export class CommitRuns extends React.Component {
 
     render() {
         let sha = truncate(this.props.commit['id'], {length: 7, omission: ''})
+        let sha_created_at = moment(this.props.commit['timestamp']).fromNow()
         let message = truncate(this.props.commit['message'], {length: 80})
         let runs = this.props.runs.map(run => {
             return {id: run['id'], name: prettifyName(run['name'])}
         })
-        return <div>{sha} - {message}
+        return <div>{sha_created_at} - {message} {sha}
             <ul>
                 {runs.map(run => {
                         return <li key={run.id}>{run.name}</li>
@@ -23,5 +25,4 @@ export class CommitRuns extends React.Component {
             </ul>
         </div>
     }
-
 }
