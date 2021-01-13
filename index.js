076c90210066fc0b469bd0ac7f733ebed1398b4c
Ivan Sanchez <ivan@gourame.com>
2021-01-13 10:58:43 +0000

    Fetch workflow runs

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class HelloMessage extends React.Component {
+class WorkflowsRuns extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        fetch("https://api.github.com/repos/http4k/http4k/actions/runs")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result['workflow_runs']
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
     render() {
-        return <div>Hello {this.props.name}</div>;
+        const { error, isLoaded, items } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <ul>
+                    {items.map(item => (
+                        <li key={item.id}>
+                            {item.name}
+                        </li>
+                    ))}
+                </ul>
+            );
+        }
     }
 }
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
+
+ReactDOM.render(<WorkflowsRuns/>, mountNode);
