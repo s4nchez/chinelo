91af3d0c0c48d766d6af7946ba92bad12092ee65
Ivan Sanchez <ivan@gourame.com>
2021-01-13 11:10:24 +0000

    Extract WorkflowRuns.js

--- index.js
+++ index.js
@@ -1,64 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-class WorkflowsRuns extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            error: null,
-            isLoaded: false,
-            items: []
-        };
-    }
-
-    componentDidMount() {
-        this.loadWorkflowRuns();
-        this.timer = setInterval(()=> this.loadWorkflowRuns(), 10000);
-    }
-
-    componentWillUnmount() {
-        this.timer = null
-    }
-
-    loadWorkflowRuns() {
-        fetch(`https://api.github.com/repos/${this.props.repo}/actions/runs`)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    this.setState({
-                        isLoaded: true,
-                        items: result['workflow_runs']
-                    });
-                },
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
-    }
-
-    render() {
-        const {error, isLoaded, items} = this.state;
-        if (error) {
-            return <div>Error: {error.message}</div>;
-        } else if (!isLoaded) {
-            return <div>Loading...</div>;
-        } else {
-            return (
-                <ul>
-                    {items.map(item => (
-                        <li key={item.id}>
-                            {item.name}
-                        </li>
-                    ))}
-                </ul>
-            );
-        }
-    }
-}
+import {WorkflowRuns} from "./src/main/js/WorkflowRuns";
 
 const mountNode = document.getElementById("app");
 
-ReactDOM.render(<WorkflowsRuns repo="http4k/http4k"/>, mountNode);
+ReactDOM.render(<WorkflowRuns repo="http4k/http4k"/>, mountNode);
---
+++ src/main/js/WorkflowRuns.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+export class WorkflowRuns extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
+
+    componentDidMount() {
+        this.loadWorkflowRuns();
+        this.timer = setInterval(() => this.loadWorkflowRuns(), 10000);
+    }
+
+    componentWillUnmount() {
+        this.timer = null
+    }
+
+    loadWorkflowRuns() {
+        fetch(`https://api.github.com/repos/${this.props.repo}/actions/runs`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result['workflow_runs']
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const {error, isLoaded, items} = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            return (
+                <ul>
+                    {items.map(item => (
+                        <li key={item.id}>
+                            {item.name}
+                        </li>
+                    ))}
+                </ul>
+            );
+        }
+    }
+}
