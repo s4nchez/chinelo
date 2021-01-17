720b872b7d8f962ee338bfee63901df35a473765
Ivan Sanchez <ivan@gourame.com>
2021-01-17 10:35:17 +0000

    add image

--- docs/chinelo.e31bb0bc.js
+++ docs/chinelo.e31bb0bc.js
@@ -47817,7 +47817,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59400" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64295" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
